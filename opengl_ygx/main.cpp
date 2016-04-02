519f9cbc2a93528d7363cac1805e7e14ff282dca
Ying Gaoxuan <Yinggx@yingdemacbook-pro.local>
2016-04-02 12:42:46 +0800

    Initial Commit

---
+++ opengl_ygx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl_ygx
+//
+//  Created by Ying Gaoxuan on 16/4/2.
+//  Copyright © 2016年 Ying Gaoxuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
